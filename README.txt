10b3f0e7ed22ec8efa43b74c1e9fccf1ee0e6660
AllenChang <iamallenchang@gmail.com>
2017-03-29 13:19:16 +0800

    delete .header img update css3 animation
    2017/3/29
    delete .header img update css3 animation

--- README.txt
+++ README.txt
@@ -1,2 +1,6 @@
 js/todo-server.js  與  todo資料夾
 在有模擬server的狀態下才能使用
+
+2017/3/29
+刪除header圖片
+修改為css3 animation
--- js/main.js
+++ js/main.js
@@ -36,4 +36,22 @@ $(document).ready(function(){
 			$('.tran').removeClass('in')
 		}
 	});
+	
+	//動畫執行秒數的array
+	var durations = ['time-xs', 'time-sm', 'time-md', 'time-lg'];
+
+    //隨機的function
+	function getRandomIntInclusive(min, max) {
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+      //隨機挑出durations array 新增class控制動畫執行秒數
+	  $('.circle').each(function(index, circle){
+	    var durationIndex = getRandomIntInclusive(0,3);
+	    var durationClass = durations[durationIndex];
+	    $(circle).addClass(durationClass);
+        //依照亂數>0.5，增加class讓圓圈反轉
+	    if(Math.random()>0.5)
+	      $(circle).addClass('reverse');
+	  });
+  
 });
